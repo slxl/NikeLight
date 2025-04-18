d2de80df42e2a2641c5bee5965c0026b72359a02
Slava Khlichkin <slava@exploreshackle.com>
2025-04-18 19:55:12 +0200

    - Error view template

---
+++ NikeLight/Presentation/Shared/ErrorView.swift
@@ -0,0 +1,36 @@
+//
+//  ErrorView.swift
+//  NikeLight
+//
+//  Created by Slava Khlichkin on 18/04/2025.
+//
+
+import SwiftUI
+
+struct ErrorView: View {
+    let error: Error
+    let retryAction: () -> Void
+
+    var body: some View {
+        VStack {
+            Text("An Error Occured")
+                .font(.title)
+
+            Text(error.localizedDescription)
+                .font(.callout)
+                .multilineTextAlignment(.center)
+                .padding(.bottom, 40).padding()
+
+            Button(
+                action: retryAction,
+                label: { Text("Retry").bold() }
+            )
+        }
+    }
+}
+
+#Preview {
+    ErrorView(error: NSError(domain: "", code: 0, userInfo: [
+        NSLocalizedDescriptionKey: "Something went wrong"]),
+    retryAction: {})
+}
